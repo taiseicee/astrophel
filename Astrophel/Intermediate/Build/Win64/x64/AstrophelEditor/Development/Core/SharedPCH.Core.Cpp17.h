07fcb34b9e66d3b914685866321576e58bb9c5ae
Tai <taiseicee@gmail.com>
2024-02-19 22:40:55 -0500

    Removed Files According to new GitIgnore

--- Astrophel/Intermediate/Build/Win64/x64/AstrophelEditor/Development/Core/SharedPCH.Core.Cpp17.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/sugiyat/Documents/GitHub/Unreal/astrophel/Astrophel/Intermediate/Build/Win64/x64/AstrophelEditor/Development/Core/SharedDefinitions.Core.Cpp17.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Astrophel/Intermediate/Build/Win64/x64/AstrophelEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/sugiyat/Documents/GitHub/Unreal/astrophel/Astrophel/Intermediate/Build/Win64/x64/AstrophelEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
